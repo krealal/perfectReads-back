20047fb172ee299c31224681839353549ad0dad2
Kevin Real <kevinreal8994@gmail.com>
2022-03-07 17:02:42 +0100

    add error middlewares to indexserver.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const { notFoundError, fatalError } = require("./middlewares/errors/error");
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(helmet());
+
+app.use(notFoundError);
+app.use(fatalError);
 
 module.exports = app;
