4dbcebb1d74933f72633ef305dd95fdcafe6367c
Kevin Real <kevinreal8994@gmail.com>
2022-03-07 16:06:07 +0100

    install helmet and add to index.js in server

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const helmet = require("helmet");
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(helmet());
 
 module.exports = app;
