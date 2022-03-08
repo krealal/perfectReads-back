9ad36ef8112d1bd2cc8dd3434cbb258d1659b03b
Kevin Real <kevinreal8994@gmail.com>
2022-03-08 11:15:36 +0100

    configure sonarscanner

--- src/server/middlewares/errors/error.test.js
+++ src/server/middlewares/errors/error.test.js
@@ -16,6 +16,23 @@ describe("Given a notFoundError function", () => {
       expect(mockedRes.json).toHaveBeenCalled();
     });
   });
+  describe("When it receives an error without code nor message", () => {
+    test("Then it should call method json with and show error /Unknown error/", () => {
+      const mockResponse = () => {
+        const res = {};
+        res.status = jest.fn().mockReturnValue(res);
+        res.json = jest.fn().mockReturnValue(res);
+        return res;
+      };
+
+      const error = {};
+
+      const mockedRes = mockResponse();
+      fatalError(error, null, mockedRes, null);
+
+      expect(mockedRes.json).toHaveBeenCalled();
+    });
+  });
   describe("When it receives an error without status and a response", () => {
     test("Then it should call method json with fatal error ", () => {
       const mockResponse = () => {
@@ -29,6 +46,24 @@ describe("Given a notFoundError function", () => {
         message: "error",
         code: null,
       };
+
+      const mockedRes = mockResponse();
+      fatalError(error, null, mockedRes, null);
+
+      expect(mockedRes.json).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it receives an error without code nor message", () => {
+    test("Then it should call method json with and show error /Unknown error/", () => {
+      const mockResponse = () => {
+        const res = {};
+        res.status = jest.fn().mockReturnValue(res);
+        res.json = jest.fn().mockReturnValue(res);
+        return res;
+      };
+
+      const error = {};
 
       const mockedRes = mockResponse();
       fatalError(error, null, mockedRes, null);
