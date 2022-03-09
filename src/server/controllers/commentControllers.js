8a8a29ff3d1ade6bb84ba4f49bdca48f78040c3f
Kevin Real <kevinreal8994@gmail.com>
2022-03-09 11:21:05 +0100

    create controller for components

---
+++ src/server/controllers/commentControllers.js
@@ -0,0 +1,14 @@
+const Comment = require("../../db/models/Comment");
+
+const listComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find();
+    res.status(200).json(comments);
+  } catch (error) {
+    next(new Error("No comments"));
+  }
+};
+
+module.export = {
+  listComments,
+};
