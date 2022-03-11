cc389cb2427c52bd925f2d823cd79eb387031a8a
Kevin Real <kevinreal8994@gmail.com>
2022-03-11 15:26:04 +0100

    add corsOption with api repo

--- src/server/index.js
+++ src/server/index.js
@@ -5,9 +5,13 @@ const helmet = require("helmet");
 const { notFoundError, fatalError } = require("./middlewares/errors/error");
 const router = require("./routes/commentsRouter");
 
+const corsOptions = {
+  origin: process.env.SERVER_RENDER,
+};
+
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(helmet());
