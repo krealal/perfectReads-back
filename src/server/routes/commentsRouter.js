b5a05bc355dcac6d4f53225f828231af806e66a5
Kevin Real Alejo <kevinreal8994@gmail.com>
2022-03-12 20:23:08 +0100

    add endpont /new-post with createReview controller

--- src/server/routes/commentsRouter.js
+++ src/server/routes/commentsRouter.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   listComments,
   deleteReview,
+  createReview,
 } = require("../controllers/commentControllers");
 
 const router = express.Router();
 
 router.get("/all", listComments);
 router.delete("/:id", deleteReview);
+router.post("/new-post", createReview);
 
 module.exports = router;
