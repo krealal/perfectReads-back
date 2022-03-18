c0a5cc3f3aed0057751ef9a9de2ec09fec0123b2
Kevin Real <kevinreal8994@gmail.com>
2022-03-18 18:48:26 +0100

    create userController for userlogin

---
+++ src/server/controllers/userController.js
@@ -0,0 +1,31 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../../db/models/User");
+
+const userLogin = async (req, res, next) => {
+  const { username, password } = req.body;
+  const findUser = await User.findOne({ username });
+
+  if (!findUser) {
+    const error = new Error("User not found");
+    error.code = 401;
+    return next(error);
+  }
+
+  const rightPassword = await bcrypt.compare(password, findUser.password);
+
+  if (!rightPassword) {
+    const error = new Error("Invalid password");
+    error.code = 401;
+    return next(error);
+  }
+
+  const UserData = {
+    name: findUser.name,
+    id: findUser.id,
+  };
+  const token = jwt.sign(UserData, process.env.JWT_SECRET);
+  return res.json({ token });
+};
+
+module.exports = { userLogin };
--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const { notFoundError, fatalError } = require("./middlewares/errors/error");
-const router = require("./routes/commentsRouter");
+const commentRouter = require("./routes/commentsRouter");
 
 const app = express();
 
@@ -12,7 +12,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(helmet());
 
-app.use("/reviews", router);
+app.use("/reviews", commentRouter);
 
 app.use(notFoundError);
 app.use(fatalError);
