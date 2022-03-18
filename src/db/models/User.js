afbd976c60917aaddd10961431612e7d3b9cd169
Kevin Real <kevinreal8994@gmail.com>
2022-03-18 17:48:05 +0100

    create user model

---
+++ src/db/models/User.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = model("User", userSchema, "users");
+
+module.exports = User;
