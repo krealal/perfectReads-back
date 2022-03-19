1265ffcf39936516c618557bfa29f4cc18023852
Kevin Real <kevinreal8994@gmail.com>
2022-03-19 12:26:09 +0100

    create endpoint /user with userRouter on it

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const { notFoundError, fatalError } = require("./middlewares/errors/error");
 const commentRouter = require("./routes/commentsRouter");
+const userRouter = require("./routes/userRouter");
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(helmet());
 
+app.use("/user", userRouter);
 app.use("/reviews", commentRouter);
 
 app.use(notFoundError);
